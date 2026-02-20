efe40e1af16b0d21b33afe8238530cb1d4fa1103
orhan504-collab <orhan_504@hotmail.com>
2026-02-21 02:54:52 +0300

    Update MainActivity.kt

--- app/src/main/java/com/turkbox/tv/MainActivity.kt
+++ app/src/main/java/com/turkbox/tv/MainActivity.kt
@@ -1,14 +1,11 @@
-// Bu kod kanalları ekranda liste halinde gösterir
+package com.turkbox.tv
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
-        // Kanal listesini GitHub'dan çekme ve ekrana basma işlemi burada yapılır
-        setupUI()
-    }
-    
-    private fun setupUI() {
-        // Android TV (Leanback) arayüzü bileşenleri
+        // Şimdilik boş bir ekran açsın
     }
 }
