cf828fd5cd5f12b4f5d88fec4de73ba2be2a64f8
orhan504-collab <orhan_504@hotmail.com>
2026-02-21 04:04:17 +0300

    Create CardPresenter.kt

---
+++ app/src/main/java/com/turkbox/tv/CardPresenter.kt
@@ -0,0 +1,27 @@
+package com.turkbox.tv
+
+import android.view.ViewGroup
+import androidx.leanback.widget.ImageCardView
+import androidx.leanback.widget.Presenter
+import androidx.core.content.ContextCompat
+
+class CardPresenter : Presenter() {
+    override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
+        val cardView = ImageCardView(parent.context).apply {
+            isFocusable = true
+            isFocusableInTouchMode = true
+            infoVisibility = ImageCardView.CARD_REGION_VISIBLE_ALWAYS
+        }
+        return ViewHolder(cardView)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
+        val channel = item as Channel
+        val cardView = viewHolder.view as ImageCardView
+        cardView.titleText = channel.name
+        cardView.setMainImageDimensions(313, 176)
+        cardView.cardType = ImageCardView.CARD_TYPE_MAIN_ONLY
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder) {}
+}
