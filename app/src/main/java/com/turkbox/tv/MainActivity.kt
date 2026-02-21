7f62883c9c5eaac8537858909d1ff50e02a2d747
orhan504-collab <orhan_504@hotmail.com>
2026-02-21 12:04:01 +0300

    Update MainActivity.kt

--- app/src/main/java/com/turkbox/tv/MainActivity.kt
+++ app/src/main/java/com/turkbox/tv/MainActivity.kt
@@ -3,51 +3,25 @@ package com.turkbox.tv
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.leanback.app.BrowseSupportFragment
-import androidx.leanback.widget.*
-import org.json.JSONObject
+import androidx.leanback.widget.ArrayObjectAdapter
+import androidx.leanback.widget.HeaderItem
+import androidx.leanback.widget.ListRow
+import androidx.leanback.widget.ListRowPresenter
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Sadece ilk açılışta fragment ekle
-        if (savedInstanceState == null) {
-            val browseFragment = BrowseSupportFragment()
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.main_frame, browseFragment)
-                .commit()
-            
-            browseFragment.title = "TurkBox TV"
-            setupAdapter(browseFragment)
-        }
-    }
+        val fragment = BrowseSupportFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame, fragment)
+            .commit()
 
-    private fun setupAdapter(fragment: BrowseSupportFragment) {
+        fragment.title = "TurkBox TV"
+        
+        // Şimdilik boş bir liste gösterelim ki JSON hatası yüzünden çökmesin
         val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
-        val listRowAdapter = ArrayObjectAdapter(CardPresenter())
-
-        try {
-            // Assets klasöründeki json'ı oku
-            val jsonString = assets.open("channels.json").bufferedReader().use { it.readText() }
-            val jsonArray = JSONObject(jsonString).getJSONArray("channels")
-
-            for (i in 0 until jsonArray.length()) {
-                val obj = jsonArray.getJSONObject(i)
-                listRowAdapter.add(Channel(
-                    obj.getInt("id"),
-                    obj.getString("name"),
-                    obj.getString("url"),
-                    obj.getString("logo")
-                ))
-            }
-        } catch (e: Exception) {
-            // Hata olursa en azından bir uyarı kartı göster, uygulama çökmesin
-            listRowAdapter.add(Channel(0, "Yükleme Hatası", "", ""))
-        }
-
-        val header = HeaderItem(0, "Canlı Yayınlar")
-        rowsAdapter.add(ListRow(header, listRowAdapter))
         fragment.adapter = rowsAdapter
     }
 }
