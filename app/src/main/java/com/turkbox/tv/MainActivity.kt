a1fccdd9de7b66d89b96cc99ce37252bbced35b9
orhan504-collab <orhan_504@hotmail.com>
2026-02-21 04:04:51 +0300

    Update MainActivity.kt

--- app/src/main/java/com/turkbox/tv/MainActivity.kt
+++ app/src/main/java/com/turkbox/tv/MainActivity.kt
@@ -5,54 +5,42 @@ import androidx.fragment.app.FragmentActivity
 import androidx.leanback.app.BrowseSupportFragment
 import androidx.leanback.widget.*
 import org.json.JSONObject
-import java.io.InputStream
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            val fragment = BrowseSupportFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.main_frame, fragment).commit()
-            
-            fragment.title = "TurkBox TV - Canlı Yayınlar"
-            setupChannels(fragment)
-        }
+        val fragment = BrowseSupportFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.main_frame, fragment).commit()
+        
+        fragment.title = "TurkBox TV"
+        fragment.headersState = BrowseSupportFragment.HEADERS_ENABLED
+        
+        setupAdapter(fragment)
     }
 
-    private fun setupChannels(fragment: BrowseSupportFragment) {
+    private fun setupAdapter(fragment: BrowseSupportFragment) {
         val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
-        val cardPresenter = VerticalGridPresenter() // Basit bir liste yapısı için
+        val cardPresenter = CardPresenter()
+        val listRowAdapter = ArrayObjectAdapter(cardPresenter)
 
-        // JSON'dan kanalları oku
-        val channels = loadChannelsFromJSON()
-        val listRowAdapter = ArrayObjectAdapter(CardPresenter())
-        channels.forEach { listRowAdapter.add(it) }
+        // Assets içindeki JSON'ı oku
+        val jsonString = assets.open("channels.json").bufferedReader().use { it.readText() }
+        val jsonArray = JSONObject(jsonString).getJSONArray("channels")
 
-        val header = HeaderItem(0, "Tüm Kanallar")
+        for (i in 0 until jsonArray.length()) {
+            val obj = jsonArray.getJSONObject(i)
+            listRowAdapter.add(Channel(
+                obj.getInt("id"),
+                obj.getString("name"),
+                obj.getString("url"),
+                obj.getString("logo")
+            ))
+        }
+
+        val header = HeaderItem(0, "Canlı Kanallar")
         rowsAdapter.add(ListRow(header, listRowAdapter))
         fragment.adapter = rowsAdapter
-    }
-
-    private fun loadChannelsFromJSON(): List<Channel> {
-        val channelList = mutableListOf<Channel>()
-        try {
-            val inputStream: InputStream = assets.open("channels.json")
-            val json = inputStream.bufferedReader().use { it.readText() }
-            val jsonObject = JSONObject(json)
-            val jsonArray = jsonObject.getJSONArray("channels")
-
-            for (i in 0 until jsonArray.length()) {
-                val item = jsonArray.getJSONObject(i)
-                channelList.add(Channel(
-                    item.getInt("id"),
-                    item.getString("name"),
-                    item.getString("url"),
-                    item.getString("logo")
-                ))
-            }
-        } catch (e: Exception) { e.printStackTrace() }
-        return channelList
     }
 }
