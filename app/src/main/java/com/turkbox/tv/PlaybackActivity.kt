1840fb11f44fc11e51c160fb7568b36e4ed73463
orhan504-collab <orhan_504@hotmail.com>
2026-02-21 14:52:12 +0300

    Update PlaybackActivity.kt

--- app/src/main/java/com/turkbox/tv/PlaybackActivity.kt
+++ app/src/main/java/com/turkbox/tv/PlaybackActivity.kt
@@ -1,9 +1,12 @@
 package com.turkbox.tv
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.StyledPlayerView
 
 class PlaybackActivity : FragmentActivity() {
@@ -16,24 +19,26 @@ class PlaybackActivity : FragmentActivity() {
         val playerView = findViewById<StyledPlayerView>(R.id.player_view)
         val videoUrl = intent.getStringExtra("CHANNEL_URL") ?: ""
 
-        if (videoUrl.isNotEmpty()) {
-            player = ExoPlayer.Builder(this).build().apply {
-                playerView.player = this
-                val mediaItem = MediaItem.fromUri(videoUrl)
-                setMediaItem(mediaItem)
-                prepare()
-                play()
-            }
+        player = ExoPlayer.Builder(this).build().apply {
+            playerView.player = this
+            
+            // Hata Dinleyici Ekle
+            addListener(object : Player.Listener {
+                override fun onPlayerError(error: PlaybackException) {
+                    Toast.makeText(this@PlaybackActivity, "Yayın Hatası: ${error.localizedMessage}", Toast.LENGTH_LONG).show()
+                }
+            })
+
+            val mediaItem = MediaItem.fromUri(videoUrl)
+            setMediaItem(mediaItem)
+            prepare()
+            playWhenReady = true
         }
     }
 
-    override fun onPause() {
-        super.onPause()
-        player?.pause()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onStop() {
+        super.onStop()
         player?.release()
+        player = null
     }
 }
