33e364e6356e84103ae44e298665130f1320c18c
orhan504-collab <orhan_504@hotmail.com>
2026-02-21 12:31:46 +0300

    Update CardPresenter.kt

--- app/src/main/java/com/turkbox/tv/CardPresenter.kt
+++ app/src/main/java/com/turkbox/tv/CardPresenter.kt
@@ -3,14 +3,14 @@ package com.turkbox.tv
 import android.view.ViewGroup
 import androidx.leanback.widget.ImageCardView
 import androidx.leanback.widget.Presenter
-import androidx.core.content.ContextCompat
+import android.graphics.Color
 
 class CardPresenter : Presenter() {
     override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
         val cardView = ImageCardView(parent.context).apply {
             isFocusable = true
             isFocusableInTouchMode = true
-            infoVisibility = ImageCardView.CARD_REGION_VISIBLE_ALWAYS
+            setBackgroundColor(Color.DKGRAY) // Görünür olması için renk ekledik
         }
         return ViewHolder(cardView)
     }
@@ -19,7 +19,7 @@ class CardPresenter : Presenter() {
         val channel = item as Channel
         val cardView = viewHolder.view as ImageCardView
         cardView.titleText = channel.name
-        cardView.contentText = "Canlı Yayın"
+        cardView.contentText = "Kanalı İzle"
         cardView.setMainImageDimensions(313, 176)
     }
 
