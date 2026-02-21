be00957909f7eb561ba8b8ba8babf51520f7050f
orhan504-collab <orhan_504@hotmail.com>
2026-02-21 03:22:36 +0300

    Update MainActivity.kt

--- app/src/main/java/com/turkbox/tv/MainActivity.kt
+++ app/src/main/java/com/turkbox/tv/MainActivity.kt
@@ -1,1 +1,11 @@
+package com.turkbox.tv
 
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
