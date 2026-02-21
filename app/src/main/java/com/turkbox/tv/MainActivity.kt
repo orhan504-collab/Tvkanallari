d56571f6b8db3ba447d9210890f079b13e0e8a39
orhan504-collab <orhan_504@hotmail.com>
2026-02-21 03:31:11 +0300

    Update MainActivity.kt

--- app/src/main/java/com/turkbox/tv/MainActivity.kt
+++ app/src/main/java/com/turkbox/tv/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Kanal yükleme kodlarını buraya ekleyeceğiz
     }
 }
