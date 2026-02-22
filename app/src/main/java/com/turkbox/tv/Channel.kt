19d42d271089ceccbb65e3b3ba742f3db18ccfc7
orhan504-collab <orhan_504@hotmail.com>
2026-02-23 00:14:52 +0300

    Update Channel.kt

--- app/src/main/java/com/turkbox/tv/Channel.kt
+++ app/src/main/java/com/turkbox/tv/Channel.kt
@@ -3,7 +3,8 @@ package com.turkbox.tv
 import com.google.gson.annotations.SerializedName
 
 data class Channel(
+    var id: Int = 0, // Kanal numarası için ekledik
     @SerializedName("name") var name: String,
     @SerializedName("url") var url: String,
-    @SerializedName("logo") var logo: String? = null // Logo eklemek istersen hazır olur
+    @SerializedName("logo") var logo: String? = null
 )
